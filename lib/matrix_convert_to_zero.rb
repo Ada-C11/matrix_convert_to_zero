636e74a76b01a512004b5fc70c5f4445b0fe92d1
Elise Pham <elise.pham88@gmail.com>
2019-03-29 20:03:37 -0700

    complete matrix_convert_to_zero

--- lib/matrix_convert_to_zero.rb
+++ lib/matrix_convert_to_zero.rb
@@ -3,8 +3,33 @@
 # If any number is found to be 0, the method updates all the numbers in the
 # corresponding row as well as the corresponding column to be 0.
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(m x n) + O(m + n)
+# Space complexity: O(m + n)
+require "set"
+require "awesome_print"
+
 def matrix_convert_to_zero(matrix)
-  raise NotImplementedError
+  rows = Set.new
+  cols = Set.new
+
+  for i in 0...matrix.length
+    for j in 0...matrix[0].length
+      if matrix[i][j] == 0
+        rows.add(i)
+        cols.add(j)
+      end
+    end
+  end
+
+  rows.each do |r|
+    for c in 0...matrix[0].length
+      matrix[r][c] = 0
+    end
+  end
+
+  cols.each do |c|
+    for r in 0...matrix.length
+      matrix[r][c] = 0
+    end
+  end
 end
