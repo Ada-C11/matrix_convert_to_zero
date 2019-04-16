f809138afb8692993b0eea96d59e0aba0407aca2
Shubha Rajan <shubharajan94@gmail.com>
2019-04-15 21:16:05 -0700

    refactored to have O(1) space complexity!

--- lib/matrix_convert_to_zero.rb
+++ lib/matrix_convert_to_zero.rb
@@ -4,36 +4,51 @@ require "pry"
 # If any number is found to be 0, the method updates all the numbers in the
 # corresponding row as well as the corresponding column to be 0.
 
-# Time complexity: O (m * n) where m and n are the dimensions of the array
+# Time complexity: O(1) because the first row and column of the matrix are used as buffer space. The only new variables created are i, j, first_row_zero, and first_column_zero
 # Space complexity: O (m + n) where m and n are the dimensions of the matrix.
 def matrix_convert_to_zero(matrix)
   i = 0
-  zero_rows = Array.new(matrix.length, 1)
-  zero_columns = Array.new(matrix[0].length, 1)
-
   # Record locations of zeros
   while i < matrix.length
     j = 0
     while j < matrix[i].length
       if matrix[i][j] == 0
-        zero_rows[i] = 0
-        zero_columns[j] = 0
+        matrix[i][0] = 0
+        matrix[0][j] = 0
+        (first_row_zero = true) if (i == 0)
+        (first_column_zero = true) if (j == 0)
       end
       j += 1
     end
     i += 1
   end
 
-  # Change columns and rows
-  i = 0
+  # Change inner rows + columns
+  i = 1
   while i < matrix.length
-    j = 0
+    j = 1
     while j < matrix[i].length
-      if (zero_rows[i] == 0) || (zero_columns[j] == 0)
+      if (matrix[i][0] == 0) || (matrix[0][j] == 0)
         matrix[i][j] = 0
       end
       j += 1
     end
     i += 1
+  end
+
+  if first_row_zero
+    j = 0
+    while j < matrix[0].length
+      matrix[0][j] = 0
+      j += 1
+    end
+  end
+
+  if first_column_zero
+    i = 0
+    while i < matrix.length
+      matrix[i][0] = 0
+      i += 1
+    end
   end
 end
