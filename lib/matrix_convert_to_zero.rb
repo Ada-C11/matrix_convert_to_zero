722d6cd2a56d7aca53640492ac733a42428eee4b
sjscotton <sj.scotton@gmail.com>
2019-04-29 16:12:18 -0700

    added complexity notes

--- lib/matrix_convert_to_zero.rb
+++ lib/matrix_convert_to_zero.rb
@@ -3,8 +3,8 @@
 # If any number is found to be 0, the method updates all the numbers in the
 # corresponding row as well as the corresponding column to be 0.
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n^2) where n is the number of items in the matrix
+# Space complexity: O(1) constant, regardless of the size of the matrix, a constant number of fixed sixed variables are needed.
 def matrix_convert_to_zero(matrix)
   rows = matrix.size
   columns = matrix[0].size
@@ -22,7 +22,6 @@ def matrix_convert_to_zero(matrix)
         columns.times { |i| matrix[row][i] = 0 unless matrix[row][i] == 2 }
         rows.times { |i| matrix[i][column] = 0 unless matrix[i][column] == 2 }
         matrix[row][column] = 0
-        # puts "#{matrix}"
       end
     end
   end
