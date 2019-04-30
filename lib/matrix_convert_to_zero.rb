2b8e0ef8246f67302e3820f6907ea35c2ff03d2d
Ariana Bray <aribray@gmail.com>
2019-04-29 19:16:06 -0700

    passed all tests

--- lib/matrix_convert_to_zero.rb
+++ lib/matrix_convert_to_zero.rb
@@ -3,8 +3,26 @@
 # If any number is found to be 0, the method updates all the numbers in the
 # corresponding row as well as the corresponding column to be 0.
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n*m), where n is the number of rows and m is the number of columns
+# Space complexity: O(n + m), where n is the number of rows in the matrix and m is the number of columns in the matrix
 def matrix_convert_to_zero(matrix)
-  raise NotImplementedError
+  return nil if matrix.empty?
+
+  rows = matrix.size
+  columns = matrix[0].size
+  row_set = []
+  col_set = []
+
+  rows.times do |row|
+    columns.times do |column|
+      row_set << row if matrix[row][column].zero?
+      col_set << column if matrix[row][column].zero?
+    end
+  end
+
+  rows.times do |row|
+    columns.times do |column|
+      matrix[row][column] = 0 if row_set.include?(row) || col_set.include?(column)
+    end
+  end
 end
