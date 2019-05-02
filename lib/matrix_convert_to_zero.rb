df66dc83715bd25ddf00dd0f4c9bd296c0391ba9
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-05-01 19:24:17 -0700

    first attempt

--- lib/matrix_convert_to_zero.rb
+++ lib/matrix_convert_to_zero.rb
@@ -6,5 +6,37 @@
 # Time complexity: ?
 # Space complexity: ?
 def matrix_convert_to_zero(matrix)
-  raise NotImplementedError
+  outer_indexes = []
+  inner_indexes = []
+  first_loop = 0
+
+  matrix.each do |arr|
+    arr.each_with_index do |num, inner_index|
+      if num == 0
+        if !outer_indexes.include?(outer_index)
+          outer_indexes << outer_index
+        end
+        if !inner_indexes.include?(inner_index)
+          inner_indexes << inner_index
+        end
+      end
+    end
+  end
+
+  matrix.each_with_index do |arr, outer_index|
+    if outer_indexes.include?(outer_index)
+      arr.each do |num|
+        if num == 1
+          num = 0
+        end
+      end
+    end
+    arr.each_with_index do |num, inner_index|
+      if inner_indexes.include?(inner_index) && num == 1
+        arr[inner_index] = 0
+      end
+    end
+  end
+
+  return matrix
 end
