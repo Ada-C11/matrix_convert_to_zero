91d98ba837b7746a0259152762e117bf34d6f52e
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-05-01 19:47:05 -0700

    passes tests, has comments

--- lib/matrix_convert_to_zero.rb
+++ lib/matrix_convert_to_zero.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 # Updates the input matrix based on the following rules:
 # Assumption/ Given: All numbers in the matrix are 0s or 1s
 # If any number is found to be 0, the method updates all the numbers in the
@@ -5,12 +7,29 @@
 
 # Time complexity: ?
 # Space complexity: ?
+# def initialize_matrix(rows, columns)
+#   # create the matrix using the rows and columns
+#   matrix = Array.new(rows) { Array.new(columns) }
+
+#   # initialize the matrix
+#   rows.times do |row|
+#     columns.times do |column|
+#       matrix[row][column] = 1
+#     end
+#   end
+
+#   return matrix
+# end
+
+# matrix = initialize_matrix(3, 5)
+# matrix[1][3] = 0
+# matrix[2][4] = 0
+
 def matrix_convert_to_zero(matrix)
   outer_indexes = []
   inner_indexes = []
-  first_loop = 0
 
-  matrix.each do |arr|
+  matrix.each_with_index do |arr, outer_index|
     arr.each_with_index do |num, inner_index|
       if num == 0
         if !outer_indexes.include?(outer_index)
@@ -25,9 +44,9 @@ def matrix_convert_to_zero(matrix)
 
   matrix.each_with_index do |arr, outer_index|
     if outer_indexes.include?(outer_index)
-      arr.each do |num|
+      arr.each_with_index do |num, inner_index|
         if num == 1
-          num = 0
+          arr[inner_index] = 0
         end
       end
     end
@@ -40,3 +59,5 @@ def matrix_convert_to_zero(matrix)
 
   return matrix
 end
+
+# matrix_convert_to_zero(matrix)
