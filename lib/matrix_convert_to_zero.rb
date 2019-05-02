131e494e8afa6c85932a0e827de0c69872244d90
Erica Norris <norrise120@gmail.com>
2019-05-01 22:03:19 -0700

    created method, seems to work, but tests not passing?

--- lib/matrix_convert_to_zero.rb
+++ lib/matrix_convert_to_zero.rb
@@ -6,5 +6,41 @@
 # Time complexity: ?
 # Space complexity: ?
 def matrix_convert_to_zero(matrix)
-  raise NotImplementedError
+  cols_with_zeros = []
+  rows_with_zeros = []
+
+  matrix.each_with_index do |row, i|
+    row.each_with_index do |col_num, j|
+      if col_num == 0
+        cols_with_zeros.push(j)
+        rows_with_zeros.push(i)
+      end
+    end
+  end
+
+  cols_with_zeros.each do |col|
+    matrix.length.times do |x|
+      matrix[x][col] = 0
+    end
+  end
+
+  rows_with_zeros.each do |row|
+    matrix.first.each do |col|
+      matrix[row][col] = 0
+    end
+  end
+
+  return matrix
 end
+
+# matrix = [
+#   [1, 0, 1, 0],
+#   [1, 1, 1, 1],
+#   [1, 0, 0, 1],
+# ]
+
+# puts "#{matrix_convert_to_zero(matrix)}"
+
+# 0 0 0 0
+# 1 0 0 0
+# 0 0 0 0
