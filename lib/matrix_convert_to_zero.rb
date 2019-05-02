a4bb23150b7822f638e8db0b74ec5d799a26fc6d
Amy Wyatt <amy.wyatt12@outlook.com>
2019-05-01 21:31:14 -0700

    working solution 1

--- lib/matrix_convert_to_zero.rb
+++ lib/matrix_convert_to_zero.rb
@@ -6,5 +6,43 @@
 # Time complexity: ?
 # Space complexity: ?
 def matrix_convert_to_zero(matrix)
-  raise NotImplementedError
+  i = 0
+  while matrix[i] != nil
+    j = 0
+    while matrix[i][j] != nil
+      if matrix[i][j] == 0
+        n = 0
+        while matrix[n] != nil
+          if matrix[n][j] == 1
+            matrix[n][j] = "X"
+          end
+          n += 1
+        end
+
+        m = 0
+        while matrix[i][m] != nil
+          if matrix[i][m] == 1
+            matrix[i][m] = "X"
+          end
+          m += 1
+        end
+      end
+      j += 1
+    end
+    i += 1
+  end
+
+  i = 0
+  while matrix[i] != nil
+    j = 0
+    while matrix[i][j] != nil
+      if matrix[i][j] == "X"
+        matrix[i][j] = 0
+      end
+      j += 1
+    end
+    i += 1
+  end
+
+  return matrix
 end
