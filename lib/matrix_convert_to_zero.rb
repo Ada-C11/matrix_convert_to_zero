032c7301e8e7b91a2d2b32d6b430ff5f0ee55632
laneia <laneiamurray@gmail.com>
2019-06-25 10:02:35 -0700

    all tests passing

--- lib/matrix_convert_to_zero.rb
+++ lib/matrix_convert_to_zero.rb
@@ -1,10 +1,43 @@
-# Updates the input matrix based on the following rules:
 # Assumption/ Given: All numbers in the matrix are 0s or 1s
 # If any number is found to be 0, the method updates all the numbers in the
 # corresponding row as well as the corresponding column to be 0.
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(m * n)
+# Space complexity: O(n)
+
 def matrix_convert_to_zero(matrix)
-  raise NotImplementedError
+  column_zero = 1
+  rows = matrix.length
+  columns = matrix[0].length
+
+  for i in 0...rows
+    if matrix[i][0] == 0
+      column_zero = 0
+    end
+
+    for j in 1...columns
+      if matrix[i][j] == 0
+        matrix[i][0] = matrix[0][j] = 0
+      end
+    end
+  end
+
+  i = rows - 1
+
+  while i >= 0
+    j = columns - 1
+
+    while j >= 1
+      if matrix[i][0] == 0 || matrix[0][j] == 0
+        matrix[i][j] = 0
+      end
+      j -= 1
+    end
+
+    if column_zero == 0
+      matrix[i][0] = 0
+    end
+
+    i -= 1
+  end
 end
